1e0f1d7df39853ae3932de7e6227722a5325b50e
Tuomas Honkala <tuho@cs.helsinki.fi>
2016-03-20 07:27:26 +0200

    badge lisätty

--- src/main/java/ohtu/ohtuvarasto/Main.java
+++ src/main/java/ohtu/ohtuvarasto/Main.java
@@ -54,6 +54,6 @@ public class Main {
         saatiin = mehua.otaVarastosta(-32.9);
         System.out.println("saatiin " + saatiin);
         System.out.println("Mehuvarasto: " + mehua);
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!!");
     }
 }
